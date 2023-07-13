1a6999737a0de9b968dab1f75ad0857357ee7501
jaime32 <jamers4590@gmail.com>
2023-07-13 06:33:14 -0700

    updates mongoDB challenge

--- api/index.js
+++ api/index.js
@@ -4,13 +4,17 @@ const mongoose = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const app = express();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const salt = bcrypt.genSaltSync(10);
+const secret = 'asef32rewfffefewfwfwfef';
 
-app.use(cors());
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser());
 
-mongoose.connect('mongodb+srv://jamers4590:WaeoOhxO31HfWiB6@cluster0.ap1nrnk.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://jamers4590:7ONj3zBtvnmeJpTu@cluster0.0tgurg9.mongodb.net/?retryWrites=true&w=majority');
 
 app.post('/register', async(req,res) =>{
     const {username,password} = req.body;
@@ -24,6 +28,36 @@ app.post('/register', async(req,res) =>{
         console.log(e);
         res.status(400).json(e);
     }
+});
+
+app.post('/login', async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username:username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk){
+        //logged in
+        jwt.sign({username, id:userDoc._id}, secret, {}, (err,token) => {
+            if(err) throw err;
+            res.cookie('token', token).json({
+                id:userDoc._id,
+                username,
+            });
+        });
+    } else {
+        res.status(400).json('wrong credentials');
+    }
+});
+
+app.get('/profile', (req,res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err,info) => {
+        if (err) throw err;
+        res.json(info);
+    });
+});
+
+app.post('/logout', (req,res) => {
+    res.cookie('token', '').json('ok');
 });
 
 app.listen(4000);
